172cf6f7ebd3ce8f6247fff11aa4d444b219e3ac
Tukeque <59365069+Tukeque@users.noreply.github.com>
2021-11-28 14:20:48 +0100

    delete classes

--- classes.py
+++
@@ -1,9 +0,0 @@
-from tools import *
-import compiler as c
-
-class Var:
-    def __init__(self, name: str, type: str, width: int, compiler: c.Compiler):
-        self.name     = name
-        self.type     = type
-        self.width    = width
-        self.compiler = compiler
