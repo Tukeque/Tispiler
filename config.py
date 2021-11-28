da2ceda33129ae4bcc7150abf4cc673869506cd5
Tukeque <TukequeMail@gmail.com>
2021-11-28 13:53:43 +0100

    config: remove useless stuff

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-regs   = 8 # doesnt matter if using "silk" # needs to be atleast 4 # 8 or more recommended for a better experience
 heap   = 256 # size of the heap
 bits   = 16 # bitlength of target arch
-modfix = True # just replaces # with M for mod's emulator to work
-run    = "ram" # can be "ram" or "rom"
 stack  = 256 # size of the stack in the header
-arch   = "urcl" # arch can be "urcl" or "silk"
 debug  = True # print debugging information
