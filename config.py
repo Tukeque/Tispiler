1f2ef007a2f3fcb77dcfd5ae8f2c570206cdb6b5
Tukeque <TukequeMail@gmail.com>
2021-11-29 21:44:23 +0100

    config.py: fix bad formatting lol

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-heap   = 256 # size of the heap
-bits   = 16 # bitlength of target arch
-stack  = 256 # size of the stack in the header
-debug  = True # print debugging information
+heap  = 256 # size of the heap
+bits  = 16 # bitlength of target arch
+stack = 256 # size of the stack in the header
+debug = True # print debugging information
