f65200cbd4c32d85838de634e2cf35f0d4581a87
Tukeque <TukequeMail@gmail.com>
2022-01-01 23:18:58 +0100

    cls.py: Fix and add classes

--- cls.py
+++ cls.py
@@ -1,9 +1,16 @@
 from tools import *
-import compiler as c
+from parse import ParserToken
 
 class Var:
-    def __init__(self, name: str, type: str, width: int, compiler):
+    def __init__(self, name: str, type: str, width: int):
         self.name     = name
         self.type     = type
         self.width    = width
-        self.compiler: c.Compiler = compiler
+
+class Func:
+    def __init__(self, name: str, func_type: str, args: list[ParserToken], code: ParserToken, ret_type: str = "void"):
+        self.name = name
+        self.func_type = func_type
+        self.args = args
+        self.code = code
+        self.ret_type = ret_type
