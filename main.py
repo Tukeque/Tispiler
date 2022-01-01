7be1b401ca2c25ed9a7779315433bc68f8711950
Tukeque <TukequeMail@gmail.com>
2022-01-01 23:42:59 +0100

    main.py: fix Compiler.compile() call

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ expr = parser.parse()
 
 #* compiler
 compiler = Compiler(parser)
-compiler.compile(expr, big=True)
+compiler.compile(expr, in_func=False, big=True)
 
 #* output
 compiler.output(out)
